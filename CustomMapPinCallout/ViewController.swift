5480f280963edc53abc8a74570491875a974a8b4
Marcelo Sampaio <marcelo@artlogica.com.br>
2018-07-30 17:50:25 -0300

    checkpoint commit 👻

--- CustomMapPinCallout/ViewController.swift
+++ CustomMapPinCallout/ViewController.swift
@@ -5,6 +5,7 @@
 //  Created by Marcelo on 7/30/18.
 //  Copyright © 2018 Marcelo. All rights reserved.
 //
+// Reference article: https://digitalleaves.com/blog/2016/12/building-the-perfect-ios-map-ii-completely-custom-annotation-views/
 
 import UIKit
 
